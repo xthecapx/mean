dafb21b30f7f312d8dad589844fcd2a58f7d7a77
Cristian Marquez <cristian@fullstacklabs.co>
2019-02-22 17:01:42 -0500

    Unit testing get

--- server/server.spec.js
+++ server/server.spec.js
@@ -1,3 +1,43 @@
-it('funciona', () => {
-  expect(true).toBe(true);
+const express = require('express');
+const logger = require('morgan');
+const http = require('http');
+const PinsRouter = require('./routes/pins');
+const Pins = require('./models/Pins');
+const request = require('request');
+const app = express();
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use('/api', PinsRouter.router);
+app.set('port', 3000);
+
+describe('Testing Router', () => {
+  let server;
+
+  beforeAll(() => {
+    server = http.createServer(app);
+    server.listen(3000);
+  })
+
+  afterAll(() => {
+    server.close();
+  });
+
+  describe('GET', () => {
+    // GET 200
+    it('200 and find pin', done => {
+      const data = [{ id: 1 }];
+      spyOn(Pins, 'find').and.callFake(callBack => {
+        callBack(false, data);
+      });
+
+      request.get('http://localhost:3000/api', (error, response, body) => {
+        expect(response.statusCode).toBe(200);
+        expect(JSON.parse(response.body)).toEqual([{ id: 1 }]);
+        done();
+      })
+    })
+
+    // GET 500
+  })
 })
