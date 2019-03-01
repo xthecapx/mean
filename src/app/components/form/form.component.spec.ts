ae55d6e7fc6174ff7ee38ce513b984895cc21443
Cristian Marquez <cristian@fullstacklabs.co>
2019-03-01 12:08:40 -0500

    26.form-module-observables

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -73,4 +73,30 @@ fdescribe('FormComponent', () => {
       expect(Object.keys(assets.controls)).toEqual(['0', '1']);
     });
   });
+
+  describe('When deleteAsset', () => {
+    it('Should remove the form control', () => {
+      const assets = <FormArray>component.secondFormGroup.get('assets');
+
+      component.addAsset();
+      component.deleteAsset(0);
+
+      expect(Object.keys(assets.controls)).toEqual([]);
+    });
+  });
+
+  describe('When savePins is executed', () => {
+    it('Should navigate to pins view', () => {
+      const navigate = spyOn((<any>component).navigate, 'goToPins');
+      const open = spyOn((<any>component).snackBar, 'open').and.callThrough();
+
+      component.savePin();
+
+      expect(navigate).toHaveBeenCalled();
+      console.log('working');
+      expect(open).toHaveBeenCalledWith('Your pin is saved, Redirecting ...', 'Cool!', {
+        duration: 2000
+      });
+    });
+  });
 });
