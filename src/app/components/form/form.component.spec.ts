e310452708676ed83a0ccdb71b55e6acc45ca70e
Cristian Marquez <cristian@fullstacklabs.co>
2019-03-01 12:06:36 -0500

    24.form-module-configuration

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -1,16 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { RepositoryService } from 'src/app/services/repository.service';
+import { of } from 'rxjs';
+import { NavigationService } from 'src/app/services/navigation.service';
+import { MatSnackBar } from '@angular/material';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule, FormArray } from '@angular/forms';
 
-describe('FormComponent', () => {
+class RepositoryServiceStub {
+  savePins() {
+    return of(true);
+  }
+}
+
+class NavigationServiceStub {
+  goToPins() {}
+}
+
+class MatSnackBarStub {
+  open() {
+    return {
+      afterDismissed: () => {
+        return of(true);
+      }
+    };
+  }
+}
+
+fdescribe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
-    })
-    .compileComponents();
+      declarations: [FormComponent],
+      providers: [
+        { provide: RepositoryService, useClass: RepositoryServiceStub },
+        { provide: NavigationService, useClass: NavigationServiceStub },
+        { provide: MatSnackBar, useClass: MatSnackBarStub }
+      ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+      imports: [ReactiveFormsModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
