f76868246285a4a57c2154aef7a4a362953f6d0c
Cristian Marquez <cristian@fullstacklabs.co>
2019-03-01 16:25:06 -0500

    32.navigation service

--- src/app/services/navigation.service.spec.ts
+++ src/app/services/navigation.service.spec.ts
@@ -1,12 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NavigationService } from './navigation.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('NavigationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('NavigationService', () => {
+  let service: NavigationService;
+
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [NavigationService],
+      imports: [RouterTestingModule]
+    })
+  );
+
+  beforeEach(() => {
+    service = TestBed.get(NavigationService);
+  });
 
   it('should be created', () => {
-    const service: NavigationService = TestBed.get(NavigationService);
     expect(service).toBeTruthy();
+  });
+
+  it('should navigate to pins', () => {
+    const navigate = spyOn((<any>service).router, 'navigate');
+
+    service.goToPins();
+
+    expect(navigate).toHaveBeenCalledWith(['/app/pins']);
+  });
+
+  it('should navigate to edit mode', () => {
+    const navigate = spyOn((<any>service).router, 'navigate');
+
+    service.goToEditMode();
+
+    expect(navigate).toHaveBeenCalledWith(['/app/add']);
   });
 });
