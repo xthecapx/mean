14b0107bc06c24ae23c693d2b1ca69289bc3c170
Cristian Marquez <cristian@fullstacklabs.co>
2019-03-01 14:21:55 -0500

    27.menu-events-emitter

--- src/app/components/menu/menu.component.spec.ts
+++ src/app/components/menu/menu.component.spec.ts
@@ -1,16 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import { By } from '@angular/platform-browser';
 
-describe('MenuComponent', () => {
+fdescribe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
-    })
-    .compileComponents();
+      declarations: [MenuComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -20,6 +20,18 @@ describe('MenuComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const title = fixture.debugElement.query(By.css('h1'));
+
+    expect(title.nativeElement.innerHTML).toBe('eLearning Management System');
+  });
+
+  it('Testing output', () => {
+    const val = true;
+
+    component.clicked.subscribe(result => {
+      expect(result).toBe(val);
+    });
+
+    component.clicked.next(val);
   });
 });
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {}
+export class MenuComponent {
+  @Output() clicked = new EventEmitter<boolean>();
+}
